4ddb3433fb751bfbb9953f2dd1a1bdc59b3ff1f5
gh-lenny <Leonardo1147@outlook.it>
2024-11-18 17:15:30 +0100

    Eliminato cartella gioco vecchia

--- gioco/levels/level.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready():
-	$player.position = Vector2(State.px,State.py)
--- gioco/script/state.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var Dialogue_Window = false
-var Minigame = false 
-var MgDone = false
-var px = 0
-var py = 0
